cee64477114f4268ea38ea37d5df8de94f893411
Vel-sama <65117005+Vel-sama@users.noreply.github.com>
2023-03-15 20:27:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var video = document.getElementById("myVideo");
 var btn = document.getElementById("btn");
-const toggleBtn = document.querySelector('.toggle__button')
-const toggleIcon = document.querySelector('.toggle__button i')
-const dropDownMenu = document.querySelector('.dropdown__menu')
+const toggleBtn = document.querySelector('.toggle__btn');
+const toggleIcon = document.querySelector('.toggle__btn i');
+const dropDownMenu = document.querySelector('.dropdown__menu');
+
+toggleBtn.onclick = function () {
+  dropDownMenu.classList.toggle('open');
+  const isOpen = dropDownMenu.classList.contains('open');
+
+  toggleIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+};
 
 function myFunction() {
   if (video.paused) {
@@ -14,10 +21,3 @@ function myFunction() {
   }
 }
 
-toggleBtn.onclick = function () {
-  dropDownMenu.classList.toggle('open')
-  const isOpen = dropDownMenu.classList.contains('open')
-
-  toggleIcon.classList = isOpen ? 'fa-solid fa-xmark' : 'fa-solid fa-bars'
-
-}
