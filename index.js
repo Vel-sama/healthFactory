9b4de4dd1c3807dfdc92a17b28285f9f9dfe3f7e
Vel-sama <65117005+Vel-sama@users.noreply.github.com>
2023-04-12 16:15:39 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -22,29 +22,29 @@ function myFunction() {
 }
 
 
-document.querySelectorAll('.carousel').forEach( carousel => {
-  const items = carousel.querySelectorAll('.carousel__item');
-  const buttonsFromHtml = Array.from(items, () => {
-    return `<span class="carousel__button"></span>`
-  });
-  carousel.insertAdjacentHTML("beforeend", `
-  <div class="carousel__nav">
-    ${buttonsFromHtml.join('')}
-  </div>
-  `);
-  const buttons = carousel.querySelectorAll('.carousel__button');
-  buttons.forEach((button, i)  => {
-    button.addEventListener('click', () => {
-      items.forEach(item => item.classList.remove('carousel__item--selected'));
-      buttons.forEach(item => item.classList.remove('carousel__button--selected'));
+// document.querySelectorAll('.carousel').forEach( carousel => {
+//   const items = carousel.querySelectorAll('.carousel__item');
+//   const buttonsFromHtml = Array.from(items, () => {
+//     return `<span class="carousel__button"></span>`
+//   });
+//   carousel.insertAdjacentHTML("beforeend", `
+//   <div class="carousel__nav">
+//     ${buttonsFromHtml.join('')}
+//   </div>
+//   `);
+//   const buttons = carousel.querySelectorAll('.carousel__button');
+//   buttons.forEach((button, i)  => {
+//     button.addEventListener('click', () => {
+//       items.forEach(item => item.classList.remove('carousel__item--selected'));
+//       buttons.forEach(item => item.classList.remove('carousel__button--selected'));
 
-      items[i].classList.add('carousel__item--selected');
-      buttons[i].classList.add('carousel__button--selected');
-    })
+//       items[i].classList.add('carousel__item--selected');
+//       buttons[i].classList.add('carousel__button--selected');
+//     })
     
-  })
-  items[0].classList.add('carousel__item--selected');
-  buttons[0].classList.add('carousel__button--selected');
-})
+//   })
+//   items[0].classList.add('carousel__item--selected');
+//   buttons[0].classList.add('carousel__button--selected');
+// })
 
 
