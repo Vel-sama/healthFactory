2b28e2aabb0f59e27de46beabe85fb8ba88a14e4
Vladislav Diachenko <vladislav.dyachenko00@gmail.com>
2023-05-23 10:35:24 +0300

    change footer and doctors section

--- index.js
+++ index.js
@@ -57,19 +57,28 @@ window.onload = function () {
 
 let phone1 = document.getElementById("phone-1");
 let phone2 = document.getElementById("phone-2");
+let phone3 = document.getElementById("phone-3");
+let phone4 = document.getElementById("phone-4");
 let citySelect = document.getElementById("city");
 let adress = document.getElementById("adress-1");
+let adress2 = document.getElementById("adress-2");
 
 citySelect.addEventListener("change", function() {
   let selectedCity = citySelect.value;
     if (selectedCity === "Vishneve") {
         phone1.innerText = "+38 (097) 2211022";
-        phone2.innerText = "  +38 (073) 2211022";
+        phone2.innerText = "+38 (073) 2211022";
+        phone3.innerText = "+38 (097) 2211022";
+        phone4.innerText = "+38 (073) 2211022";
         adress.innerText = "вул. Машинобудівників 17– А";
+        adress2.innerText = "вул. Машинобудівників 17– А";
     } else if (selectedCity === "Kiyv") {
         phone1.innerText = "+38 (066) 2211002";
         phone2.innerText = "+38 (068) 2211002";
-        adress.innerText = "Просп. Героїв Сталінграду 2Г"
+        phone3.innerText = "+38 (066) 2211002";
+        phone4.innerText = "+38 (068) 2211002";
+        adress.innerText = "Просп. Героїв Сталінграду 2Г";
+        adress2.innerText = "Просп. Героїв Сталінграду 2Г"
     }
 });
   
